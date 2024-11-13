09d1588c18f9325986302519a1272e44d27361ec
Nemo De Chabalier <nde-chab@paul-f5Ar3s8.clusters.42paris.fr>
2024-11-13 19:51:54 +0100

    pars texture finished

--- Srcs/Parsing/verrif_RGB.c
+++ Srcs/Parsing/verrif_RGB.c
@@ -6,7 +6,7 @@
 /*   By: nde-chab <nde-chab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 19:02:46 by nde-chab          #+#    #+#             */
-/*   Updated: 2024/11/13 19:09:00 by nde-chab         ###   ########.fr       */
+/*   Updated: 2024/11/13 19:49:11 by nde-chab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,16 @@ char	*RGB_alloc(char *str)
 	new = ft_calloc(sizeof(char), count_alloc_texture(str) + 2);
 	if (!new)
 		return (NULL);
-	while(i < 4 && str[i])
+	while (i < 4 && str[i])
 		new[j++] = str[i++];
-	while (str[1])
+	new[j++] = ' ';
+	while (str[i])
+	{
+		if (str[i] != ' ')
+			new[j++] = str[i];
+		i++;
+	}
+	return (new);
 }
 
 int	RGB_cut(t_file *file)
@@ -33,4 +40,35 @@ int	RGB_cut(t_file *file)
 	int	i;
 
 	i = 0;
+	while (file->RGB[i])
+	{
+		file->RGB[i] = RGB_alloc(file->RGB[i]);
+		if (!file->RGB[i])
+		{
+			if (i == 1)
+				free(file->RGB[0]);
+			return (FALSE);
+		}
+		i++;
+	}
+	if (!strncmp(file->RGB[0], file->RGB[1], 2))
+		return (printf("Error\nsame RGB\n"));
+	return (TRUE);
 }
+
+//int	RGB_agrs(t_file *file)
+//{
+//	int	i;
+
+//	i = 0;
+//	while (file->RGB[i])
+//	{
+		
+//	}
+//}
+
+//int	RGB_verrif(t_file *file)
+//{
+//	if (!RGB_cut(file))
+//		return (FALSE);
+//}
